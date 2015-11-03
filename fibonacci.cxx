dc66b8e289b715676240b27bb323c88c972dfe27
farnaz farzam <farnaz@linux-bv06.site>
2015-11-04 00:33:18 +0100

    fibonacci.cxx

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main (){
+int N;
+int a=0;
+int b=1;
+int c;
+cout << "N" << endl; 
+cin >> N;
+for(int i=0; i<N-1; i++){
+c=a+b;
+a=b;
+b=c;
+}
+cout << c << endl;
+return 0;
+}
